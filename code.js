17f5963a8e79a1cd8a1bd5d52f70a3b209d2160f
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-03-23 16:19:39 -0500

    unit tests for edge cases

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(input) {
-    return "Hello " + input + "!";
+function sayHello() {
+    return "Hello, World!";
 }
 
--- tests.js
+++ tests.js
@@ -23,11 +23,7 @@ describe('sayHello', function() {
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
-    // it('should return the string "Hello, Pat!" when executed', function () {
-    //     expect(sayHello("Pat")).toBe( "Hello, Pat!");
-    // });
-    // it('should return the string "Hello, Jane!" when executed', function () {
-    //     expect(sayHello("Jane")).toBe("Hello, Jane!")
-    // });
-
+    it('should return a string when called', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
 });
