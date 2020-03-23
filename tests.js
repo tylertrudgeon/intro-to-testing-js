117e40efde92eb8cec7dd7eedded1ed1faa2975a
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-03-23 15:21:35 -0500

    add the first test and solution for intro-to-testing
    clear
    
    '

--- tests.js
+++ tests.js
@@ -13,3 +13,11 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+//Unit tests for sayHello function
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe('function');
+    });
+});
